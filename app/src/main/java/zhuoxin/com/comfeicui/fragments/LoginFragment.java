d0f6601a9298719ecca3d70fbe3511eda7a3f12a
wukaifighting <826275981@qq.com>
2016-11-03 16:10:40 +0800

    11.3.2登录注册

--- app/src/main/java/zhuoxin/com/comfeicui/fragments/LoginFragment.java
+++ app/src/main/java/zhuoxin/com/comfeicui/fragments/LoginFragment.java
@@ -23,53 +23,53 @@ import zhuoxin.com.comfeicui.interfacea.HttpInterface;
  * Created by Administrator on 2016/11/2.
  */
 
-public class LoginFragment extends Fragment implements View.OnClickListener,HttpInterface{
+public class LoginFragment extends Fragment implements View.OnClickListener, HttpInterface {
     Button mBtn_zc;
     Button mBtn_wjmm;
     Button mBtn_ddl;
     EditText mEdt_name;
     EditText mEdit_password;
     RequestQueue requestQueue;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.denglu,container,false);
+        return inflater.inflate(R.layout.denglu, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mEdt_name= (EditText) view.findViewById(R.id.edit_dl_name);
-        mEdit_password= (EditText) view.findViewById(R.id.edt_dl_password);
-        mBtn_zc= (Button) view.findViewById(R.id.btn_denglu_zhuce);
-        mBtn_wjmm= (Button) view.findViewById(R.id.btn_denglu_wjmm);
-        mBtn_ddl= (Button) view.findViewById(R.id.btn_denglu_dl);
+        mEdt_name = (EditText) view.findViewById(R.id.edit_dl_name);
+        mEdit_password = (EditText) view.findViewById(R.id.edt_dl_password);
+        mBtn_zc = (Button) view.findViewById(R.id.btn_denglu_zhuce);
+        mBtn_wjmm = (Button) view.findViewById(R.id.btn_denglu_wjmm);
+        mBtn_ddl = (Button) view.findViewById(R.id.btn_denglu_dl);
         mBtn_ddl.setOnClickListener(this);
         mBtn_wjmm.setOnClickListener(this);
         mBtn_zc.setOnClickListener(this);
-    requestQueue= Volley.newRequestQueue(getActivity());
+        requestQueue = Volley.newRequestQueue(getActivity());
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_denglu_zhuce:
-                RegistFragment fragment=new RegistFragment();
-                FragmentTransaction fragmentTransaction=getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.center,fragment);
+                RegistFragment fragment = new RegistFragment();
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.center, fragment);
                 fragmentTransaction.commit();
                 break;
             case R.id.btn_denglu_wjmm:
-                ForgetFragment wjmmFragment=new ForgetFragment();
-                FragmentTransaction fragmentT=getFragmentManager().beginTransaction();
-                fragmentT.replace(R.id.center,wjmmFragment);
+                ForgetFragment wjmmFragment = new ForgetFragment();
+                FragmentTransaction fragmentT = getFragmentManager().beginTransaction();
+                fragmentT.replace(R.id.center, wjmmFragment);
                 fragmentT.commit();
                 break;
-            case  R.id.btn_denglu_dl:
-                  String  name=mEdt_name.getText().toString();
-                  String password=mEdit_password.getText().toString();
-                new HttpUtil().startconnection(HttpInfo.BASE_URL+HttpInfo.START,name,password,this,requestQueue);
-
+            case R.id.btn_denglu_dl:
+                String name = mEdt_name.getText().toString();
+                String password = mEdit_password.getText().toString();
+                new HttpUtil().startconnection(HttpInfo.BASE_URL + HttpInfo.START, name, password, this, requestQueue);
 
 
         }
@@ -77,6 +77,13 @@ public class LoginFragment extends Fragment implements View.OnClickListener,Http
 
     @Override
     public void getresponse(String message) {
-        Log.e("messagr","message"+message);
+
+        Log.e("messagr", "message" + message);
+//        try {
+//            JSONObject jsonObject=new JSONObject(message);
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
     }
 }
